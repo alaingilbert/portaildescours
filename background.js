1e094d017e34337df57624931ff9d5f987a0b2ae
alaingilbert <alain.gilbert.15@gmail.com>
2012-01-12 13:43:33 -0500

    remove useless code

--- background.js
+++ background.js
@@ -1,9 +1,3 @@
-var requestFilter = {
-   urls: [
-      "<all_urls>"
-   ]
-};
-
 chrome.experimental.webRequest.onBeforeSendHeaders.addListener(function(details) {
    var headers = details.requestHeaders;
    for(var i = 0, l = headers.length; i < l; ++i) {
@@ -13,4 +7,4 @@ chrome.experimental.webRequest.onBeforeSendHeaders.addListener(function(details)
       }
    }
    return { requestHeaders: headers };
-}, requestFilter, ['requestHeaders','blocking']);
+}, { urls: ["<all_urls>"] }, ['requestHeaders', 'blocking']);
